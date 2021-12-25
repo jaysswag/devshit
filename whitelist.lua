3627bc39b6e8d4ece978742616634b88ed646fd2
2985289549859 <66913721+catvskid@users.noreply.github.com>
2021-12-25 11:13:57 -0300

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,14 @@
+local groupID = 12892505
+local plr = game.Players.LocalPlayer
+
+repeat wait() until plr.Character
+
+if plr:IsInGroup(groupID) then
+print "big cock and balls"
+
+else
+
+if plr:IsInGroup(groupID) == false then
+game.Players.LocalPlayer:Kick("ur fucking idiot u better dont cuz im about to kill ur whole family")
+    end
+end
